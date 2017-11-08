1848ddae4225da9b02c77a19be784d70b0a58645
byz1014 <byz1014@163.com>
2017-11-08 16:02:04 +0800

    1108

--- app/src/main/java/com/example/byz/studyandroid/TestActivity.java
+++ app/src/main/java/com/example/byz/studyandroid/TestActivity.java
@@ -1,16 +1,18 @@
 package com.example.byz.studyandroid;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Handler;
-import android.os.Message;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import com.example.byz.studyandroid.base.BaseActivity;
-import com.example.byz.studyandroid.utils.BitmapUtil;
 
 import org.greenrobot.eventbus.Subscribe;
 
@@ -19,75 +21,143 @@ import org.greenrobot.eventbus.Subscribe;
  */
 
 public class TestActivity extends BaseActivity implements View.OnClickListener {
-    TextView test;
-    ImageView iv_back;
-    SeekBar sb_bar;
 
     @Override
     public int onLayout() {
         return R.layout.layout_test;
     }
-int progress;
     @Override
     public void init() {
-        test = (TextView) findViewById(R.id.test);
-        iv_back = (ImageView)findViewById(R.id.iv_back) ;
-        sb_bar = (SeekBar)findViewById(R.id.sb_bar);
-        test.setOnClickListener(this);
-          image = BitmapFactory.decodeResource(getResources(),R.mipmap.back_test2);
+        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        mToast.setGravity(Gravity.CENTER, 0, 0);
 
-        sb_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        // App Logo
+//        mToolbar.setLogo(R.drawable.app_icon);
+        // 主标题,默认为app_label的名字
+        mToolbar.setTitle("Title");
+        mToolbar.setTitleTextColor(Color.YELLOW);
+        // 副标题
+        mToolbar.setSubtitle("Sub title");
+        mToolbar.setSubtitleTextColor(Color.parseColor("#80ff0000"));
+        //侧边栏的按钮
+        mToolbar.setNavigationIcon(R.mipmap.ic_launcher_round);
+        //取代原本的actionbar
+        setSupportActionBar(mToolbar);
+        //设置NavigationIcon的点击事件,需要放在setSupportActionBar之后设置才会生效,
+        //因为setSupportActionBar里面也会setNavigationOnClickListener
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if(i<=1){
-                    i = 1;
-                }
-                progress = i;
-                Message message = new Message();
-                message.arg1 = progress;
-                handler.sendMessage(message);
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//
-//                    }
-//                }).start();
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public void onClick(View v) {
+                mToast.setText("click NavigationIcon");
+                mToast.show();
             }
         });
-
+        //设置toolBar上的MenuItem点击事件
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_edit:
+                        mToast.setText("click edit");
+                        break;
+                    case R.id.action_share:
+                        mToast.setText("click share");
+                        break;
+                    case R.id.action_overflow:
+                        //弹出自定义overflow
+                        popUpMyOverflow();
+                        return true;
+                }
+                mToast.show();
+                return true;
+            }
+        });
+        //ToolBar里面还可以包含子控件
+        mToolbar.findViewById(R.id.btn_diy).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mToast.setText("点击自定义按钮");
+                mToast.show();
+            }
+        });
+        mToolbar.findViewById(R.id.tv_title).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mToast.setText("点击自定义标题");
+                mToast.show();
+            }
+        });
     }
-    Bitmap image;
+
     @Override
     @Subscribe
     public void onEventMainThread(String str) {
 
     }
 
+    Toolbar mToolbar;
+    Toast mToast;
+    PopupWindow mPopupWindow;
+    //如果有Menu,创建完后,系统会自动添加到ToolBar上
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.test:
-
-                break;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            iv_back.setImageBitmap( BitmapUtil.getBitmapUtil().fastblur(getActivity(),image,msg.arg1));
+    /**
+     * 弹出自定义的popWindow
+     */
+    public void popUpMyOverflow() {
+        //获取状态栏高度
+        Rect frame = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        //状态栏高度+toolbar的高度
+        int yOffset = frame.top + mToolbar.getHeight();
+        if (null == mPopupWindow) {
+            //初始化PopupWindow的布局
+            View popView = getLayoutInflater().inflate(R.layout.action_overflow_popwindow, null);
+            //popView即popupWindow的布局，ture设置focusAble.
+            mPopupWindow = new PopupWindow(popView,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            //必须设置BackgroundDrawable后setOutsideTouchable(true)才会有效
+            mPopupWindow.setBackgroundDrawable(new ColorDrawable());
+            //点击外部关闭。
+            mPopupWindow.setOutsideTouchable(true);
+            //设置一个动画。
+            mPopupWindow.setAnimationStyle(android.R.style.Animation_Dialog);
+            //设置Gravity，让它显示在右上角。
+            mPopupWindow.showAtLocation(mToolbar, Gravity.RIGHT | Gravity.TOP, 0, yOffset);
+            //设置item的点击监听
+            popView.findViewById(R.id.ll_item1).setOnClickListener(this);
+            popView.findViewById(R.id.ll_item2).setOnClickListener(this);
+            popView.findViewById(R.id.ll_item3).setOnClickListener(this);
+        } else {
+            mPopupWindow.showAtLocation(mToolbar, Gravity.RIGHT | Gravity.TOP, 0, yOffset);
         }
-    };
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_item1:
+                mToast.setText("哈哈");
+                break;
+            case R.id.ll_item2:
+                mToast.setText("呵呵");
+                break;
+            case R.id.ll_item3:
+                mToast.setText("嘻嘻");
+                break;
+        }
+        //点击PopWindow的item后,关闭此PopWindow
+        if (null != mPopupWindow && mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+        }
+        mToast.show();
+    }
 }
